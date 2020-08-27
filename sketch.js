6e2104ef84bbf770938cb4b1ce1b443c9e407cde
rohakyadav <67333192+rohakyadav@users.noreply.github.com>
2020-08-27 18:55:25 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var car,bananaPeel,car1,theifJewel,theifCosplay,police;
+var car_image,bananaPeel_image,car1_image,car_image,theifJewel_image,theifCosplay_image,police_image;
+var bg1,bg2,bg3;
+var backgd
+
+function preload() {
+bg1 = loadImage("sprites/bg1.png")
+}
+
+
+function setup() {
+  createCanvas(1600,1600);
+  backgd = createSprite(0,0,width,height)
+}
+
+
+function draw() {
+  background(0);
+
+  
+  
+  drawSprites();
+ 
+}
